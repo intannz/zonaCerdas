bef1b4750cf88ecb0745dad9d590d31dafd78f32
Intan Maharani <carimzar1943@gmail.com>
2026-06-20 22:35:52 +0700

    style(auth): add global footer to auth layout

--- src/shared/layouts/AuthLayout.jsx
+++ src/shared/layouts/AuthLayout.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 export default function AuthLayout() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#EFE5FF] p-6 font-['Nunito_Sans']">
-      <Outlet />
+    <div className="min-h-screen flex flex-col bg-[#EFE5FF] font-['Nunito_Sans']">
+
+      <div className="flex-grow flex items-center justify-center p-6">
+        <Outlet />
+      </div>
+
+      <Footer />
+      
     </div>
   );
 }
